02302b549ac20d4194dbc557bd17bd70a102147e
Nina Janson <janson.nin@gmail.com>
2022-06-19 11:41:45 +0200

    fix: test for check h1

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,8 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe("My First Test", () => {
+describe("test quiz app", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.contains("h1", "QUIZBOX");
   });
 });
